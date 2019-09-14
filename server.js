b85ccff8f4308ba7f8332a6b61566a556da284d5
Areum Jo <areumjo1@gmail.com>
2019-09-14 15:11:13 -0700

    Added custom-middleware

--- server.js
+++ server.js
@@ -8,13 +8,19 @@ const server = express();
 
 // GLOBAL MIDDLEWARE
 
-
 // built-in middleware
 server.use(express.json());
 
 // third part middleware
 server.use(helmet());
 server.use(logger('dev'));
+
+// custom middleware
+server.use(typeLogger);
+server.use(addName);
+//server.use(lockout);
+//server.use(moodyGatekeeper);
+
 
 // router
 server.use('/api/hubs', hubsRouter);
@@ -27,5 +33,36 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+// custom middleware
+function typeLogger(req, res, next) {
+  console.log(`${req.method} Request`);
+    // GET Request <-- our console.log fn
+  // GET /api/hubs 200 11.845 ms - 470 <-- morgan (logger)
+  next();
+};
+
+function addName(req, res, next) {
+  req.name =req.name || "Cass";
+  next();
+}
+
+function lockout(req, res, next) {
+  res.status(403).json({ message: 'API lockout!' });
+}
+
+function moodyGatekeeper(req, res, next) {
+  // it keeps you out 1/3 of the time
+  // when it decides to keep you out it sends back status 403 msg
+
+  const seconds = new Date().getSeconds();
+
+  if (seconds % 3 === 0) {
+    res.status(403).json({ message: "You shall not pass!"});
+  } else {
+    next();
+  }
+
+}
 
 module.exports = server;
